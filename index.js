24066a6a8fcb0dce1ab197aa03b8896a6ed7a2d3
mrsfarra3 <63860557+mrsfarra3@users.noreply.github.com>
2021-05-31 09:26:07 -0500

    Filled in boilerplate code to demonstrate ReactDom.render code showing input and output

---
+++ index.js
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//Takes two arguments - what you want to render, and where you want to render it.
+ReactDOM.render((<h1>Hello world</h1>), document.getElementById("root"))
